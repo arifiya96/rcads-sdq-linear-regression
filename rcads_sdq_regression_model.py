012ab4c50d615d7178837f7e28c64c154ead8ac8
Arifiya96 <59474537+arifiya96@users.noreply.github.com>
2020-09-28 13:31:06 +0100

    Add files via upload

--- rcads_sdq_regression_model.py
+++ rcads_sdq_regression_model.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np 
 import pandas as pd 
+import altair as alt
+import base64
 
 #Converting the json data to a dataframe
 df = pd.read_json('data.json')
@@ -8,18 +10,18 @@ df = pd.read_json('data.json')
 #Sidebar
 st.sidebar.header('Completion filters')
 st.sidebar.write('RCADS Clinical Ranges')
-rcads_clinical_ranges = ['High','Medium','Low']
-rcads_high = st.sidebar.checkbox(rcads_clinical_ranges[0])
-rcads_medium = st.sidebar.checkbox(rcads_clinical_ranges[1])
-rcads_low = st.sidebar.checkbox(rcads_clinical_ranges[2])
+rcads_bandings = ['Low','Medium','High']
+selected_rcads_banding = st.sidebar.multiselect('RCADS Bandings', rcads_bandings, rcads_bandings)
 st.sidebar.write('SDQ Clinical Ranges')
-sdq_clinical_ranges = ['Below Clinical Threshold','Borderline','Above Clinical Threshold']
-sdq_below = st.sidebar.checkbox(sdq_clinical_ranges[0])
-sdq_borderline = st.sidebar.checkbox(sdq_clinical_ranges[1])
-sdq_above = st.sidebar.checkbox(sdq_clinical_ranges[2])
+sdq_bandings = ['Below Clinical Threshold','Borderline','Above Clinical Threshold']
+selected_sdq_banding = st.sidebar.multiselect('SDQ Bandings', sdq_bandings, sdq_bandings)
+st.sidebar.header('SDQ Total Score Predictor')
+rcads_score = st.sidebar.slider('RCADS Total Score', 0, 130)
+sdq_predicted_score = np.around((0.09*rcads_score) + 7.95)
+st.sidebar.write('SDQ Predicted Score: ' + str(sdq_predicted_score)) 
 
 #Filtering
-
+df_selected = df[(df.rcads_banding.isin(selected_rcads_banding)) & (df.sdq_banding.isin(selected_sdq_banding))]
 
 #Raw data section
 st.title('RCADS SDQ Regression Model')
@@ -28,5 +30,13 @@ This interactive web app allows you to explore some of the RCADS and SDQ complet
 Feel free to explore some of the data and the regression model.  
 """)
 st.write('Raw Data - RCADS and SDQ scores')
-st.write('Data Dimension: ' + str(df.shape[0]) + ' rows and ' + str(df.shape[1]) + ' columns')
-st.write(df)
+st.write('Data Dimension: ' + str(df_selected.shape[0]) + ' rows and ' + str(df_selected.shape[1]) + ' columns')
+st.write(df_selected)
+
+#Linear regression model
+st.title('Linear Regression - RCADS SDQ Completions')
+scatter_chart = st.altair_chart(
+    alt.Chart(df).mark_circle(size=60).encode(x='rcads_score_anx_dep_t2',y='sdq_total_t2',tooltip=['rcads_score_anx_dep_t2','sdq_total_t2']).interactive()
+)
+st.write('Regression Equation: SDQ_Score(Y) = 0.09x + 7.95')
+
